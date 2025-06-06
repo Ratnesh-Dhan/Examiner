ca811dfe6494121a5fcb0862f7531e0dfbdb2490
Ratnesh-Dhan <ratneshdhan@gmail.com>
2025-06-06 18:26:16 +0530

    update

--- src/app/components/TestBox.tsx
+++ src/app/components/TestBox.tsx
@@ -5,7 +5,7 @@ const TestBox = () => {
     <div className="md:m-20 border rounded-lg border-slate-200">
       <div className="flex justify-between md:p-10 p-5">
         <div className="text-xl md:text-3xl">Timer</div>
-        <button className="text-xl md:text-3xl">Submit</button>
+        <button className="text-xl md:text-3xl bg-[#50d71e]">Submit</button>
       </div>
     </div>
   );
