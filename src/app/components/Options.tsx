9e8bc347fe76107092477abca132ac6fec810884
Ratnesh-Dhan <ratneshdhan@gmail.com>
2025-06-10 16:50:46 +0530

    option color when clicked.

--- src/app/components/Options.tsx
+++ src/app/components/Options.tsx
@@ -23,7 +23,9 @@ const Options = ({
 
   return (
     <div
-      className="relative flex justify-center md:m-10 m-2"
+      className={`relative flex justify-center md:m-10 m-2 cursor-pointer ${
+        answers[count - 1] === numbering ? "bg-[#255F38] text-white" : ""
+      }`}
       onClick={handleClick}
     >
       <p className="border rounded-lg px-20 py-5 md:text-lg">
